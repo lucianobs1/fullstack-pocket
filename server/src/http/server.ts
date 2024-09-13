e1409d386319eadc3f62c13c73659c3d557c8395
lucianobs1 <lucianojob1.@hotmail.com>
2024-09-13 07:30:17 -0300

    create: use-case goal

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,37 @@
 import fastify from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
 
-const app = fastify();
+import { createGoal } from '../use-cases/create-goal';
+import z from 'zod';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body;
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    });
+  }
+);
 
 app
   .listen({
@@ -8,6 +39,5 @@ app
     host: '0.0.0.0',
   })
   .then(() => {
-    console.log(process.env.DATABASE_URL);
     console.log('⭐ Server is running ⭐');
   });
